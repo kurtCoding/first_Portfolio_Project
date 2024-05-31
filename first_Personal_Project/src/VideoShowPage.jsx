df896b2dcce2c125a4f59dc5c6354f83556c8789
kurtCoding <kjulien7790@gmail.com>
2024-05-31 16:47:06 -0400

    attemtped to render episode to videoshowpage from hoohah component

--- first_Personal_Project/src/VideoShowPage.jsx
+++ first_Personal_Project/src/VideoShowPage.jsx
@@ -4,11 +4,35 @@ import { Link, useParams } from "react-router-dom";
 
 export default function VideoShowPage() {
 
+    const { id } = useParams();
+    const [episode, setEpisode] = useState(null);
+
+    useEffect(() => {
+        fetch(`https://rickandmortyapi.com/api/episode/${id}`).then((response) => response.json())
+        .then((data) => {
+            setEpisode(data)
+        })
+    }, [id])
+
+    if (!episode) {
+        return <div>Your video is Loading...</div>;
+    }
 
     return(
         <div>
             <h1>🎥</h1>
-            
+            <div>
+                <h2>{episode.name}</h2>
+                <iframe 
+                    width="560" 
+                    height="315" 
+                    src={episode.url} 
+                    title={episode.name} 
+                    frameBorder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowFullScreen>
+                </iframe>
+            </div>
         </div>
     )
 }
